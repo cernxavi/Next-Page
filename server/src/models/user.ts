1ee7fe365b347d96447197f09d4825e21961f4f8
Xavier Cerna <xaviii.4@Xaviers-MacBook-Pro.local>
2024-11-21 22:02:23 -0700

    client/pages

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -2,18 +2,14 @@ import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
 import bcrypt from 'bcrypt';
 
 interface UserAttributes {
-  id: number;
   username: string;
-  email: string;
   password: string;
 }
 
-interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+interface UserCreationAttributes extends Optional<UserAttributes, 'username'> {}
 
 export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-  public id!: number;
   public username!: string;
-  public email!: string;
   public password!: string;
 
   public readonly createdAt!: Date;
@@ -30,16 +26,7 @@ export class User extends Model<UserAttributes, UserCreationAttributes> implemen
 export function UserFactory(sequelize: Sequelize): typeof User {
   User.init(
     {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
       username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
         type: DataTypes.STRING,
         allowNull: false,
       },
