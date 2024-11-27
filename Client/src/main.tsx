329bb2dbf4919a0524aef4fa73f2415fb5f6af2c
riley <mechamriley@gmail.com>
2024-11-26 19:29:47 -0700

    saving changes before merging with main

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -2,12 +2,37 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
-import App from './App.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-// import MainPage from './pages/MainPage.tsx';
-import SearchBooks from './pages/search.tsx';
-// import RecommendBook from './pages/recommend.tsx';
+// import App from './App.tsx';
+// import ErrorPage from './pages/ErrorPage.tsx';
+// // import MainPage from './pages/MainPage.tsx';
+// import SearchBooks from './pages/search.tsx';
+// // import RecommendBook from './pages/recommend.tsx';
 
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import MainPage from './pages/MainPage';
+import Login from './pages/login';
+import MyBooks from './pages/myBooks';
+import Recommend from './pages/recommend';
+import Search from './pages/search';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         index: true,
+//         element: <SearchBooks />
+//       },
+//       {
+//         path: '/recommend',
+//         // element: <RecommendBook />
+//       }
+//     ]
+//   }
+// ]);
 const router = createBrowserRouter([
   {
     path: '/',
@@ -16,15 +41,28 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <SearchBooks />
+        element: <MainPage />
+      }, 
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/myBooks',
+        element: <MyBooks />
       },
       {
         path: '/recommend',
-        // element: <RecommendBook />
-      }
+        element: <Recommend />
+      },
+      {
+        path: '/search',
+        element: <Search />
+      },
     ]
   }
 ]);
+
 
 const rootElement = document.getElementById('root');
 if(rootElement) {
