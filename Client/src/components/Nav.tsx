321af91f1c9544c26e6b6841e1093b232e879636
riley <mechamriley@gmail.com>
2024-11-26 19:54:44 -0700

    accepted incoming changes

--- Client/src/components/Nav.tsx
+++ Client/src/components/Nav.tsx
@@ -36,33 +36,6 @@ function Navigation() {
       </Container>
     </Navbar>
   );
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand as={Link} to="/">Next Page</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link as={Link} to="/myBooks">MyBooks</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-            <Nav.Link as={Link} to="/recommend">Recommend</Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
 }
 
 export default Navigation;
