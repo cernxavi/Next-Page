dac134748b930a7eeda14d8468aef8ec844de60f
Xavier Cerna <xaviii.4@Xaviers-MacBook-Pro.local>
2024-12-01 12:08:33 -0700

    Authentication added

--- server/dist/routes/api/index.js
+++ server/dist/routes/api/index.js
@@ -1,5 +1,7 @@
 import { bookRouter } from './bookRoutes.js';
 import express from 'express';
+import { userRouter } from './userRoutes.js';
 const router = express.Router();
 router.use('/books', bookRouter);
+router.use('/users', userRouter);
 export default router;
