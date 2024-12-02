2768472f2f5ddb1c6adecaba77431142b4c28d44
Martha Watson <marthacdenzer@gmail.com>
2024-12-02 12:30:38 -0700

    cleaning up

--- server/dist/seeds/book-seeds.js
+++
@@ -1,8 +0,0 @@
-import { Book } from '../models/book.js';
-export const seedBook = async () => {
-    await Book.bulkCreate([
-        { title: 'To Kill a MockingBird', subtitle: 'A Novel', image: 'https://images-na.ssl-images-amazon.com/images/I/51ZJ2q4Z5jL._SX331_BO1,204,203,200_.jpg' },
-        { title: 'The New Annotated Dracula', subtitle: 'The Essential Text of Bram Stoker\'s Classic Novel', image: 'https://images-na.ssl-images-amazon.com/images/I/51ZJ2q4Z5jL._SX331_BO1,204,203,200_.jpg' },
-        { title: 'The Historian', subtitle: 'A Novel', image: 'https://images-na.ssl-images-amazon.com/images/I/51ZJ2q4Z5jL._SX331_BO1,204,203,200_.jpg' },
-    ], { individualHooks: true });
-};
