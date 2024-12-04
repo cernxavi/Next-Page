6e9905537d2477c81f60447d25cb13f75d90873a
riley <mechamriley@gmail.com>
2024-12-03 22:55:52 -0700

    changed connection.js to .ts and deleted the extra server.js file

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-// Set PORT dynamically via Render, default to 3001 for local development
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
