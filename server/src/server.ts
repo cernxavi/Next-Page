36dd282175dcb8f2adaa11039561afd65a8b77c6
riley <mechamriley@gmail.com>
2024-12-04 18:42:07 -0700

    updated the path for static files to correctly name Client instead of client. added @types/express dependecy

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ import { sequelize } from './models/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../../client/dist'));
+app.use(express.static('../../Client/dist'));
 
 app.use(express.json());
 app.use(routes)
