f7e3365dbd614200309a870aafc1202f25a0c0e1
riley <mechamriley@gmail.com>
2024-12-04 19:51:31 -0700

    added another line to server.ts to make the static files render

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,14 @@ const forceDatabaseRefresh = false;
 
 
 import express from 'express';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+
+// set the file and directory paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //set up the express app
 const app = express();
@@ -12,6 +17,10 @@ const PORT = process.env.PORT || 3001;
 console.log(`PORT environment variable: ${process.env.PORT}`);
 
 app.use(express.static('../../Client/dist'));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../client/dist', 'index.html'));
+});
 // import path from 'path';
 // app.use(express.static(path.resolve(__dirname, '../../Client/dist')));
 
